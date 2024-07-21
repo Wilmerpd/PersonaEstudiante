794f8266f44f974ddf29bd83d6cc2c582b16fc4b
lagar <lagar@WILMERPC>
2024-07-21 18:57:17 -0400

    Agregar archivos de proyecto.

---
+++ PersonaEstudiante/Person.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace PersonaEstudiante
+{
+    public class Person
+    {
+        public int Edad { get; set; }
+        public string Nombre { get; set; }
+        public string Ciudad { get; set; }
+
+
+        
+        public Person(string nombre, int edad, string ciudad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Ciudad = ciudad;
+        }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"Nombre: {Nombre} Edad: {Edad}, Ciudad {Ciudad} ");
+        }
+        public int CalcularEdad() 
+        { 
+            return Edad;
+        }
+    }
+    public class Estudiante : Person
+    {
+        public string Carrera { get; set; }
+        public string Matricula { get; set; }
+
+        public Estudiante(string nombre, int edad, string ciudad, string carrera, string matricula)
+           : base(nombre, edad, ciudad)
+        {
+            Carrera = carrera;
+            Matricula = matricula;
+        }
+        public void MostrarInformaciones()
+        {
+            MostrarInfo();
+            Console.WriteLine($"Carrera: {Carrera}, Matricula: {Matricula}");
+        }
+
+    }
+
+}
---
+++ PersonaEstudiante/Program.cs
@@ -0,0 +1,9 @@
+﻿using PersonaEstudiante;
+
+Person person = new Person("Wilmer", 19, "Santo Domingo");
+person.MostrarInfo();
+Console.WriteLine($"Edad en años: {person.CalcularEdad()}");
+
+
+Estudiante estudiante = new Estudiante("Jose", 12, "San Juan", "Ingeniería de Software", "2024-2022");
+estudiante.MostrarInformaciones();
